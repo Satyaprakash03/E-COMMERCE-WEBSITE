8a6a2527c84f5c160c29a1bf86eb863c85ac5b0a
SAHIL SATYAPRAKASH <sahil.satyaprakash007@gmail.com>
2025-09-28 22:05:08 +0530

    first-commit

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,29 @@
+import express from "express";
+import {
+  placeOrder,
+  placeOrderStripe,
+  allOrders,
+  userOrders,
+  updateStatus,
+  verifyStripe,
+} from "../controllers/orderController.js";
+import adminAuth from "../middleware/adminAuth.js";
+import authUser from "../middleware/auth.js";
+
+const orderRouter = express.Router();
+
+// ADMIN FEATURES
+orderRouter.post("/list", adminAuth, allOrders);
+orderRouter.post("/status", adminAuth, updateStatus);
+
+// PAYMENT FEATURES
+orderRouter.post("/place", authUser, placeOrder);
+orderRouter.post("/stripe", authUser, placeOrderStripe);
+
+// USER FEATURE
+orderRouter.post("/userorders", authUser, userOrders);
+
+// VERIFY PAYMENT
+orderRouter.post("/verifyStripe", authUser, verifyStripe);
+
+export default orderRouter;
